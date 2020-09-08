46d83a20d47403969121a1cc3d052916de17865a
yellow2041 <yellow2041@naver.com>
2020-09-08 23:40:53 +0900

    feat: 메인화면 로그인 모달창 구현
    - 아직 완성되진 않음
    - 모달창 이외의 영역을 클릭하면 모달창이 닫히도록 함
    - 프로필 메뉴도 다른영역을 클릭하면 닫히도록 함

--- myapp/public/javascripts/index.js
+++ myapp/public/javascripts/index.js
@@ -2,18 +2,43 @@ let t=document.getElementById('profile_menu_container');
 let c=document.getElementById('header_user_btn');
 
 c.addEventListener('click',function(event){
-    t.style.visibility= 'visible';
-    c.className="header_user_btn_click";
+    if(t.style.visibility==='visible'){
+        t.style.visibility = 'hidden';
+        c.className="header_user_btn";
+    }
+    else{
+        t.style.visibility= 'visible';
+        c.className="header_user_btn_click";
+    }
 });
 
 let login=document.getElementById('login');
-
+let login_modal=document.getElementById('login_modal');
 login.addEventListener('click',(event)=>{
-    location.href='/login';
+    login_modal.style.display="block";
 });
 
 let signUp=document.getElementById('signUp');
 
 signUp.addEventListener('click',(event)=>{
     location.href='/signUp';
+});
+
+
+let close_modal=document.getElementById('close_modal');
+
+close_modal.addEventListener('click',(event)=>{
+    login_modal.style.display="none";
+});
+
+document.getElementById('modal_overlay').addEventListener('click',(event)=>{
+    if(event.target.id!=='modal'&&login_modal.style.display==="block")
+        login_modal.style.display="none";
+})
+
+document.getElementById('body_div').addEventListener('click',(event)=>{
+    if(event.target.id!=='profile_menu'&&t.style.visibility=== 'visible'){
+        t.style.visibility= 'hidden';
+        c.className="header_user_btn";
+    }
 })
